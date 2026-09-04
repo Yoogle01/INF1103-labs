e5099312ee2de0f4b5b14eb9a14217ca7059f01f
YiTing <yiting@gmail.com>
2026-09-04 17:09:31 +0800

    Fun conditions

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ print("my first post")
 print("===============================")
 
 username = input("Enter Username: ")
-age = input("Enter Age: ") 
+age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
 
 print("\n Instagram Profile")
@@ -12,4 +12,7 @@ print("=======================")
 print("Username: ", username)
 print("Age: ", age)
 print("Category: ", category)
+
+if age > 40 and category == "fun" or "Fun":
+    print("You are old, what is fun for you?")
 

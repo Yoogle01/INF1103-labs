916ffdf9cb85f7bb86c74bf5b83564b2c4a33abd
YiTing <yiting@gmail.com>
2026-09-04 16:46:35 +0800

    update

--- hello.py
+++ hello.py
@@ -2,3 +2,21 @@ print("===============================")
 print("Welcome here")
 print("my first post")
 print("===============================")
+
+username = "cool_cucumber"
+bio = "Fun blogger"
+followers = 100
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+

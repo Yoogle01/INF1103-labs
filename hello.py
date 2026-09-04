1814bd78a51cee8348b4a49978c567b3e551e7c3
YiTing <yiting@gmail.com>
2026-09-04 16:59:32 +0800

    Dynamic Profiles

--- hello.py
+++ hello.py
@@ -3,19 +3,13 @@ print("Welcome here")
 print("my first post")
 print("===============================")
 
-username = "cool_cucumber"
-bio = "Fun blogger"
-followers = 100
+username = input("Enter Username: ")
+age = input("Enter Age: ") 
+category = input("Enter Content Category: ")
 
-followers += 50
-print("Day 1:", followers)
+print("\n Instagram Profile")
+print("=======================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
 
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
-
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
